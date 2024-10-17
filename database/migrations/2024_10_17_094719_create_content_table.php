ecaf422f47dcaac1d2e9f9b2be416c7bec96e9db
frankjam <frank.jamlick@gmail.com>
2024-10-17 21:02:42 +0300

    db schema update

--- database/migrations/2024_10_17_094719_create_content_table.php
+++ database/migrations/2024_10_17_094719_create_content_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('content', function (Blueprint $table) {
             $table->id();
             $table->string("name");
+            $table->unsignedBigInteger('course_id');
             $table->timestamps();
         });
     }
